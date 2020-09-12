561391b66e6ee18ab8df33887b3b0f6ba77f76b7
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-09-11 22:52:06 -0600

    Removed self error.

--- tests/test_status_endpoint.py
+++ tests/test_status_endpoint.py
@@ -7,7 +7,7 @@ import pytest
 """
 TODO: Send a GET request to our API endpoint to verify solid connection.
 """
-def test_valid_connection(self):
+def test_valid_connection():
     """
     This test will ensure that a connection has been made to our api
     returning a 200 status code.

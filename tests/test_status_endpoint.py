5a20c0e73b940104f30fc36e19fd0aec787af109
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-09-11 22:51:23 -0600

    Updated testing.

--- tests/test_status_endpoint.py
+++ tests/test_status_endpoint.py
@@ -1,13 +1,13 @@
 """
 Unit testing with python
 """
-import pytest 
+import pytest
 
 
 """
 TODO: Send a GET request to our API endpoint to verify solid connection.
 """
-def test_valid_connection():
+def test_valid_connection(self):
     """
     This test will ensure that a connection has been made to our api
     returning a 200 status code.

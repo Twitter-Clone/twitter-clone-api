70281aee938d507583a2cc05ce10199d7e89a0d0
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-09-18 15:13:18 -0600

    Added first view.

--- tcapi/views.py
+++ tcapi/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from os.path import exists
 
-# Create your views here.
+
+class PageView(TemplateView):
+    """
+    TODO: Documentation
+    """
+    def get_template_names(self):
+        template_name = self.kwargs.get('template', 'Status.html')
+        if not exists('template/' + template_name):
+            template_name = 'missing.html'
+        return template_name

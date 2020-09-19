095665190120eb36eff559e291da05b4df3490e7
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-09-19 01:22:58 -0600

    Added 6 more tests.

--- tcapi/tests.py
+++ tcapi/tests.py
@@ -5,20 +5,6 @@ class SimpleTests(SimpleTestCase):
     """
     This class tests that a connection is made to our API.
     """
-    def check_template(self, page, template):
-        """
-        TODO: Documentation
-        """
-        response = self.client.get(page)
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, template_name=template)
-
-    def test_view_index(self):
-        """
-        TODO: Documentation
-        """
-        self.check_template('/', 'index.html')
-
     def test_api_get_pass(self):
         """
         GET request to our api returning a 200 status code.
@@ -33,3 +19,44 @@ class SimpleTests(SimpleTestCase):
         response = self.client.get('/missing.html')
         self.assertEqual(response.status_code, 200)
 
+    def test_api_post_pass(self):
+        """
+        POST request to our api returning a 200 status code.
+        """
+        response = self.client.post('/index.html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_api_post_fail(self):
+        """
+        GET request to our api returning a 200 status code.
+        """
+        response = self.client.post('/missing.html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_api_put_pass(self):
+        """
+        PUT request to our api returning a 200 status code.
+        """
+        response = self.client.put('/index.html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_api_put_fail(self):
+        """
+        PUT request to our api returning a 200 status code.
+        """
+        response = self.client.put('/missing.html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_api_delete_pass(self):
+        """
+        DELETE request to our api returning a 200 status code.
+        """
+        response = self.client.delete('/index.html')
+        self.assertEqual(response.status_code, 200)
+
+    def test_api_delete_fail(self):
+        """
+        DELETE request to our api returning a 200 status code.
+        """
+        response = self.client.delete('/missing.html')
+        self.assertEqual(response.status_code, 200)

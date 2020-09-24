0b78a96cbf36694b5b25ddd113f9cb52c3fefe78
Sean Strawmatt <50216928+sstrawmatt2@users.noreply.github.com>
2020-09-24 17:28:28 -0600

    Update urls.py

--- tcapi/urls.py
+++ tcapi/urls.py
@@ -5,7 +5,6 @@ from tcapi import views
 from .views import PageView
 
 urlpatterns = [
-    path(r'admin/', admin.site.urls),
     path('', PageView.as_view(template_name="index.html")),
     path('missing/', PageView.as_view(template_name="missing.html")),
     path('<str:template>', PageView.as_view()),

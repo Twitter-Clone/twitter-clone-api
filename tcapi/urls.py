ca7e5b9e9607299538446142939cba0b168d5006
Sean Strawmatt <50216928+sstrawmatt2@users.noreply.github.com>
2020-09-24 17:08:08 -0600

    Update urls.py

--- tcapi/urls.py
+++ tcapi/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     path('', PageView.as_view(template_name="index.html")),
     path('missing/', PageView.as_view(template_name="missing.html")),
     path('<str:template>', PageView.as_view()),
+    url(r'^api/tutorials$', views.tcapi),
+    url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tcapi),
+    url(r'^api/tutorials/published$', views.tcapi)
 ]
 

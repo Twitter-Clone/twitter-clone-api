ffb13389039893fc0d1785a963d09b9d8260b6fd
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-09-24 17:34:37 -0600

    Added models for database.

--- tcapi/models.py
+++ tcapi/models.py
@@ -1,3 +1,46 @@
+```
+TODO: documentation
+```
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    ```
+    Users Table
+    ```
+    userid = models.IntegerField(max_length=8, blank=False, default='')
+    twitterhandle = models.CharField(max_length=15, blank=False, default='')
+    email = models.EmailField(max_length=254, blank=False, default='')
+    password = CharField(max_length=128, blank=False, default='') 
+
+class Posts(models.Model):
+    ```
+    Posts Table
+    ```
+    postid = models.IntegerField(max_length=8, blank=False, default='')
+    tweet = models.CharField(max_length=280, blank=False, default='')
+        
+    # Foreign key to Users table
+    userid = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+class PostReactions(models.model):
+    ```
+    PostsReactions
+    ```
+    reactionsid = models.IntegerField(max_length=8, blank=False, default='')
+    postlikes = models.IntegerField(max_length=8, blank=False, default='') 
+    postcomments = models.CharField(max_length=280, blank=False, default='') 
+    
+    # Foreign key to Posts table
+    postid = models.ForeignKey(Posts, on_delete=models.CASCASE)
+
+class CommentReplies(models.Model):
+    ```
+    CommentReplies Table
+    ```
+    commentsid = models.IntegerField(max_length=8, blank=False, default='')
+    postcomments = models.CharField(max_length=280, blank=False, default='')
+
+    # Foreign key to PostReactions table
+    reactionsid = models.ForeignKey(PostReactions, on_delete=models.CASCADE)
+    

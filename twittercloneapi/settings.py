e0fc436e6c525398b266cb57fa9ae139a3ecb87a
Doug Mellon <68387187+dougmellon@users.noreply.github.com>
2020-09-24 16:50:37 -0600

    Update settings.py
    Added CORS middleware.

--- twittercloneapi/settings.py
+++ twittercloneapi/settings.py
@@ -58,6 +58,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware'
 ]
 
 #ROOT_URLCONF = 'twittercloneapi.urls'

fb91f69e82add8e4a2aa0d032e4faaaa69b39a2e
Sean Strawmatt <50216928+sstrawmatt2@users.noreply.github.com>
2020-09-24 17:28:00 -0600

    Update urls.py

--- twittercloneapi/urls.py
+++ twittercloneapi/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls import url
+
 
 urlpatterns = [
-    path('polls/', include('polls.urls')),
+    path('polls/', include('tcapi.urls')),
     path('admin/', admin.site.urls),
 ]

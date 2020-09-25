6ebaf3b3420ec1929adb1ce11493be818eea7f49
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-09-25 11:15:50 -0600

    Fixed error in installed app.

--- twittercloneapi/settings.py
+++ twittercloneapi/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'twittercloneapi',
-    'tcapi.apps.TutorialsConfig',
+    'tcapi.apps.TcapiConfig',
     'corsheaders'
 ]
 

42bd31a93314bfd710c20f4502b3d2fdf6a632fd
Sean Strawmatt <50216928+sstrawmatt2@users.noreply.github.com>
2020-09-29 15:51:51 -0600

    Update urls.py

--- tcapi/urls.py
+++ tcapi/urls.py
@@ -7,9 +7,11 @@ from .views import PageView
 urlpatterns = [
     path('', PageView.as_view(template_name="index.html")),
     path('missing/', PageView.as_view(template_name="missing.html")),
-    #url(r'^api/tutorials$', views.tcapi),
-    #url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tcapi),
-    #url(r'^api/tutorials/published$', views.tcapi)
+    url(r'^api/tcapi$', views.user_list),
+    url(r'^api/tcapi/(?P<pk>[0-9]+)$', views.user_detail),
+#     url(r'^api/tutorials$', views.tcapi),
+#     url(r'^api/tutorials/(?P<pk>[0-9]+)$', views.tcapi),
+#     url(r'^api/tutorials/published$', views.tcapi)
     path('<str:template>', PageView.as_view()),
 ]
 

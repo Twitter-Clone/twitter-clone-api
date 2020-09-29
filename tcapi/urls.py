c833f62490c63898c196c48d7f2509fc33dd4756
Sean Strawmatt <50216928+sstrawmatt2@users.noreply.github.com>
2020-09-29 15:58:33 -0600

    Update urls.py

--- tcapi/urls.py
+++ tcapi/urls.py
@@ -1,14 +1,9 @@
-from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
 from tcapi import views
 
-from .views import PageView
-
 urlpatterns = [
-    path('', PageView.as_view(template_name="index.html")),
-    path('missing/', PageView.as_view(template_name="missing.html")),
     url(r'^api/tcapi$', views.user_list),
     url(r'^api/tcapi/(?P<pk>[0-9]+)$', views.user_detail),
-    path('<str:template>', PageView.as_view()),
+    
 ]
 

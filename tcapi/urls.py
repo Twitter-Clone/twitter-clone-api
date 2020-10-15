2145cb33b341f072698a961dd0aba4511b3720d9
Matthew Knop <knopmatthew93@gmail.com>
2020-10-15 16:18:55 -0600

    Added missing argument.

--- tcapi/urls.py
+++ tcapi/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'^api/tcapi$', views.user_list),
     url(r'^api/tcapi/(?P<pk>[0-9]+)$', views.user_detail),
     url(r'^api/tcapi$', views.post_list),
-    url(r'^api/tcapi/(?P<pk>[0-9]+)$')
+    url(r'^api/tcapi/(?P<pk>[0-9]+)$', views.post_list)
 
 ]
 

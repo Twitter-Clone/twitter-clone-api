4665ad09309ddf6a3b8b9dc818b14a2afae1127f
Matthew Knop <knopmatthew93@gmail.com>
2020-10-15 16:08:33 -0600

    Added separate database for github actions.

--- twittercloneapi/settings.py
+++ twittercloneapi/settings.py
@@ -94,17 +94,28 @@ WSGI_APPLICATION = 'twittercloneapi.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'defaultdb',
-        'USER': 'doadmin',
-        'PASSWORD': 'ibod1y2mu7ewf5hg',
-        'HOST': 'twitter-clone-db-do-user-7047576-0.a.db.ondigitalocean.com',
-        'PORT': '25060',
+if os.getenv('GITHUB_WORKFLOW'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'github-actions',
+            'USER': 'postgres',
+            'PASSWORD': 'postgres',
+            'HOST': 'localhost',
+            'PORT': '5432',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'defaultdb',
+            'USER': 'doadmin',
+            'PASSWORD': 'ibod1y2mu7ewf5hg',
+            'HOST': 'twitter-clone-db-do-user-7047576-0.a.db.ondigitalocean.com',
+            'PORT': '25060',
+        }
+    }
 
 
 # Password validation

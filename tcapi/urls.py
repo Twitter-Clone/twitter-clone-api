7713b1bedf198670cacfd79fa09c8eebc46a1da0
Doug Mellon <68387187+dougmellon@users.noreply.github.com>
2020-10-16 15:46:29 -0600

    Added URLS

--- tcapi/urls.py
+++ tcapi/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     url(r'^api/users', views.user_list),
     url(r'^api/users/(?P<pk>[0-9]+)$', views.user_detail),
     url(r'^api/posts', views.tweet_list),
+    url(r'^api/users/(?P<pk>[0-9]+)$', views.tweet_detail)
 ]

00e1807390c1145b793f98282bbeea253c643d6c
Matthew Knop <knopmatthew93@gmail.com>
2020-10-15 19:16:57 -0600

    Removed old files.

--- twittercloneapi/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import include, path
-from django.conf.urls import url
-
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^', include('tcapi.urls'))
-]

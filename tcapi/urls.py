7aaf645ce9d4af11c6cfab7c91f8a5b193c9c7c1
dougmellon <dm@dougmellon.com>
2020-10-15 22:56:02 -0600

    Fixed some URLS.

--- tcapi/urls.py
+++ tcapi/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from tcapi import views
 
 urlpatterns = [
-    url(r'^api/tcapi/users', views.user_list),
-    url(r'^api/tcapi/users/(?P<pk>[0-9]+)$', views.user_detail),
-    url(r'^api/tcapi/posts', views.tweet_list),
+    url(r'^api/users', views.user_list),
+    url(r'^api/users/(?P<pk>[0-9]+)$', views.user_detail),
+    url(r'^api/posts', views.tweet_list),
 ]

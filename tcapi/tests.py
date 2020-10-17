60fbfe131a84fd859bb598b3968b81f9e0372320
chogue1 <70293241+chogue1@users.noreply.github.com>
2020-10-16 21:08:54 -0600

    Update tests.py

--- tcapi/tests.py
+++ tcapi/tests.py
@@ -94,9 +94,10 @@ class UserTableTests(TestCase):
         User.objects.create(userid="5230033", twitterhandle="test3", email="test3@test.com", password="PASSWORD")
         User.objects.create(userid="1000073", twitterhandle="test4", email="test4@test.com", password="PASSWORD")
         
-        User.objects.get.all().delete()
+        response = User.objects.get.all().delete()
         num_users = User.objects.count()
         self.assertEqual(num_users, 0)
+        self.assertEqual(response.status_code, 302)
         
     def test_delete_user(self):
         """
@@ -107,15 +108,21 @@ class UserTableTests(TestCase):
         User.objects.create(userid="5230033", twitterhandle="test3", email="test3@test.com", password="PASSWORD")
         User.objects.create(userid="1000073", twitterhandle="test4", email="test4@test.com", password="PASSWORD")
         
-        User.objects.get(twitterhandle="test1").delete()
+        response = User.objects.get(twitterhandle = "test1").delete()
         num_users = User.objects.count()
         self.assertEqual(num_users, 3)
+        self.assertEqual(response.status_code, 302)
         
     def test_update_user(self):
         """
-        Creates and updates one user from the database
+        Creates and updates the email of one user from the database
         """
-        self.user = User.objects.create(userid="1322253", twitterhandle="test1", email="test1@test.com", password="PASSWORD")
+        User.objects.create(userid="1322253", twitterhandle="test1", email="test1@test.com", password="PASSWORD")
+        User.objects.create(userid="1325110", twitterhandle="test2", email="test2@test.com", password="PASSWORD")
+        User.objects.create(userid="5230033", twitterhandle="test3", email="test3@test.com", password="PASSWORD")
+        User.objects.create(userid="1000073", twitterhandle="test4", email="test4@test.com", password="PASSWORD")
+        
+        self.user = User.objects.get(twitterhandle = "test2")
         
         
 class TweetTableTests(TestCase):
@@ -124,9 +131,26 @@ class TweetTableTests(TestCase):
     """
     def test_add_tweet(self):
         """
-        Creates a new tweet
+        Adds a new tweet to the database
         """
-        self.tweet = Posts.objects.create(postid="1425002", tweet="test1", userid_id="1322253")
-        num_users = User.objects.count()
+        Posts.objects.create(postid="1425002", tweet="test1", userid_id="1322253")
+        
+        num_tweets = Posts.objects.count()
+        
         self.assertEqual(num_users, 1)
+        
+    def test_add_tweets(self):
+        """
+        Adds three new tweets to the database
+        """
+        Posts.objects.create(postid="1425002", tweet="test1", userid_id="1322253")
+        Posts.objects.create(postid="1492332", tweet="test2", userid_id="1374553")
+        Posts.objects.create(postid="1482002", tweet="test3", userid_id="9220253")
+        
+        num_tweets = Posts.object.count()
+        
+        self.assertEqual(num_users, 3)
+        
+    
+        
         

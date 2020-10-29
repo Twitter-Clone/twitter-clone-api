923ba04f6f50058ec0622c7df85e219f90f2e06d
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-10-29 16:52:21 -0600

    Formatted changes.

--- tcapi/models.py
+++ tcapi/models.py
@@ -1,22 +1,25 @@
 from django.db import models
 
+
 # Create your models here.
 class User(models.Model):
     """
     Users table
     """
-    userid = models.IntegerField(blank=False, default='')
-    twitterhandle = models.CharField(max_length=15, blank=False, default='')
-    email = models.EmailField(max_length=254, blank=False, default='')
-    password = models.CharField(max_length=128, blank=False, default='') 
+
+    userid = models.IntegerField(blank=False, default="")
+    twitterhandle = models.CharField(max_length=15, blank=False, default="")
+    email = models.EmailField(max_length=254, blank=False, default="")
+    password = models.CharField(max_length=128, blank=False, default="")
 
 
 class Posts(models.Model):
     """
     Posts table
     """
-    postid = models.IntegerField(blank=False, default='')
-    tweet = models.CharField(max_length=280, blank=False, default='')
+
+    postid = models.IntegerField(blank=False, default="")
+    tweet = models.CharField(max_length=280, blank=False, default="")
 
     # Foreign key to Users table
     userid = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -26,9 +29,10 @@ class PostReactions(models.Model):
     """
     PostReactions table
     """
-    reactionsid = models.IntegerField(blank=False, default='')
-    postlikes = models.IntegerField(blank=False, default='') 
-    postcomments = models.CharField(max_length=280, blank=False, default='')
+
+    reactionsid = models.IntegerField(blank=False, default="")
+    postlikes = models.IntegerField(blank=False, default="")
+    postcomments = models.CharField(max_length=280, blank=False, default="")
 
     # Foreign key to Posts table
     postid = models.ForeignKey(Posts, on_delete=models.CASCADE)
@@ -38,8 +42,9 @@ class CommentReplies(models.Model):
     """
     CommentReplies table
     """
-    commentsid = models.IntegerField(blank=False, default='')
-    postcomments = models.CharField(max_length=280, blank=False, default='')
+
+    commentsid = models.IntegerField(blank=False, default="")
+    postcomments = models.CharField(max_length=280, blank=False, default="")
 
     # Foreign key to PostReactions table
     reactionsid = models.ForeignKey(PostReactions, on_delete=models.CASCADE)

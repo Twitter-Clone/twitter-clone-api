602050cf7b4375dc49ad85099667ff79fe662b49
Matthew Adam Knop <54086826+maknop@users.noreply.github.com>
2020-10-30 02:29:41 -0600

    Fixed test cases.

--- tcapi/tests.py
+++ tcapi/tests.py
@@ -95,9 +95,11 @@ class UserTableTests(TestCase):
             password="PASSWORD",
         )
 
-        twitterhandle = User.objects.get()
+        twitterhandle = User.objects.all()
 
-        self.assertEqual(twitterhandle.status_code, 302)
+        self.assertEqual(len(twitterhandle), 3)
+
+        # self.assertEqual(twitterhandle.status_code, 302)
 
         return str(twitterhandle)
 
@@ -115,7 +117,6 @@ class UserTableTests(TestCase):
         twitterhandle = User.objects.get(twitterhandle="test1")
 
         self.assertEqual(twitterhandle, self.user)
-        self.assertEqual(twitterhandle.status_code, 302)
 
         return str(twitterhandle)
 
@@ -147,10 +148,6 @@ class UserTableTests(TestCase):
 
         self.assertEqual(twitterhandle1, self.user)
         self.assertEqual(twitterhandle2, self.user2)
-        self.assertEqual(twitterhandle1.status_code, 302)
-        self.assertEqual(twitterhandle2.status_code, 302)
-
-        return str(twitterhandle1 + " \n" + twitterhandle2)
 
     def test_delete_users(self):
         """
@@ -181,12 +178,11 @@ class UserTableTests(TestCase):
             password="PASSWORD",
         )
 
-        response = User.objects.get.all().delete()
+        User.objects.all().delete()
 
         num_users = User.objects.count()
 
         self.assertEqual(num_users, 0)
-        self.assertEqual(response.status_code, 302)
 
     def test_delete_user(self):
         """
@@ -217,10 +213,9 @@ class UserTableTests(TestCase):
             password="PASSWORD",
         )
 
-        response = User.objects.get(twitterhandle="test1").delete()
+        User.objects.get(twitterhandle="test1").delete()
         num_users = User.objects.count()
         self.assertEqual(num_users, 3)
-        self.assertEqual(response.status_code, 302)
 
     def test_update_user(self):
         """
@@ -258,7 +253,6 @@ class UserTableTests(TestCase):
         response = User.objects.get(twitterhandle="test2")
 
         self.assertEqual(response.email, "myemail2test@hotmail.com")
-        self.assertEqual(response.status_code, 302)
 
     def test_update_users(self):
         """
@@ -301,14 +295,191 @@ class UserTableTests(TestCase):
         response2 = User.objects.get(twitterhandle="test4")
 
         self.assertEqual(response.email, "myemail2test@hotmail.com")
-        self.assertEqual(response.status_code, 302)
         self.assertEqual(response2.email, "email4me@me.com")
-        self.assertEqual(response2.status_code, 302)
 
+
+class TweetTableTests(TestCase):
+    """
+    These set of tests checks various areas related to the Posts's table.
+    """
+    def test_add_tweet(self):
+        """
+        Adds a new tweet to the database
+        """
+        test_user = User.objects.create(userid="337282", twitterhandle="test1", email="test1@test.com", password="PASSWORD")
+        Posts.objects.create(postid="1425002", tweet="test1", userid=test_user)
+
+        num_tweets = Posts.objects.count()
+
+        self.assertEqual(num_tweets, 1)
+
+    def test_add_tweets(self):
+        """
+        Adds three new tweets to the database
+        """
+        test_user = User.objects.create(userid="337282", twitterhandle="test1", email="test1@test.com", password="PASSWORD")
+        Posts.objects.create(postid="1425002", tweet="test1", userid=test_user)
+        Posts.objects.create(postid="1492332", tweet="test2", userid=test_user)
+        Posts.objects.create(postid="1482002", tweet="test3", userid=test_user)
+
+        num_tweets = Posts.objects.count()
+
+        self.assertEqual(num_tweets, 3)
+
+    def test_get_alltweets(self):
+        """
+        Gets all tweets from the database
+        """
+        test_user = User.objects.create(userid="337282", twitterhandle="test1", email="test1@test.com", password="PASSWORD")
+        Posts.objects.create(postid="1482002", tweet="test1", userid=test_user)
+        Posts.objects.create(postid="1482007", tweet="test2", userid=test_user)
+        Posts.objects.create(postid="1425666", tweet="test3", userid=test_user)
+
+        response = Posts.objects.all()
+
+        self.assertEqual(len(response), 3)
+
+    def test_get_tweet(self):
+        """
+        Gets one tweet from the database
+        """
+        test_user = User.objects.create(userid="337282", twitterhandle="test1", email="test1@test.com", password="PASSWORD")
+        Posts.objects.create(postid="1485402", tweet="test1", userid=test_user)
+        Posts.objects.create(postid="1487602", tweet="test2", userid=test_user)
+        Posts.objects.create(postid="1481002", tweet="test3", userid=test_user)
+
+        response = Posts.objects.get(tweet="test2")
+
+        self.assertEqual(response.tweet, "test2")
+
+    def test_get_tweets(self):
+        """
+        Gets two tweets from the database
+        """
+        test_user = User.objects.create(userid="337282", twitterhandle="test1", email="test1@test.com", password="PASSWORD")
+        Posts.objects.create(postid="1485402", tweet="test1", userid=test_user)
+        Posts.objects.create(postid="1487602", tweet="test2", userid=test_user)
+        Posts.objects.create(postid="1481002", tweet="test3", userid=test_user)
+
+        response1 = Posts.objects.get(tweet="test1")
+        response2 = Posts.objects.get(tweet="test3")
+
+        self.assertEqual(response1.tweet, "test1")
+        self.assertEqual(response2.tweet, "test3")
+
+    def test_delete_tweet(self):
+        """
+        Deletes one tweet from the database
+        """
+        test_user = User.objects.create(userid="337282", twitterhandle="test1", email="test1@test.com", password="PASSWORD")
+        Posts.objects.create(postid="1485402", tweet="test1", userid=test_user)
+        Posts.objects.create(postid="1487602", tweet="test2", userid=test_user)
+        Posts.objects.create(postid="1481002", tweet="test3", userid=test_user)
+        Posts.objects.create(postid="1999999", tweet="test4", userid=test_user)
+
+        Posts.objects.get(tweet="test1").delete()
+        num_posts = Posts.objects.count()
+
+        self.assertEqual(num_posts, 3)
+
+    def test_delete_tweets(self):
+        test_user = User.objects.create(userid="337282", twitterhandle="test1", email="test1@test.com", password="PASSWORD")
+        Posts.objects.create(postid="1485402", tweet="test1", userid=test_user)
+        Posts.objects.create(postid="1487602", tweet="test2", userid=test_user)
+        Posts.objects.create(postid="1481002", tweet="test3", userid=test_user)
+        Posts.objects.create(postid="1999999", tweet="test4", userid=test_user)
+
+        Posts.objects.get(tweet="test1").delete()
+        Posts.objects.get(tweet="test4").delete()
+
+        num_posts = Posts.objects.count()
+
+        self.assertEqual(num_posts, 2)
+
+    def test_delete_all(self):
+        """
+        Deletes all tweets from the database
+        """
+        test_user = User.objects.create(userid="337282", twitterhandle="test1", email="test1@test.com", password="PASSWORD")
+        Posts.objects.create(postid="1485402", tweet="test1", userid=test_user)
+        Posts.objects.create(postid="1487602", tweet="test2", userid=test_user)
+        Posts.objects.create(postid="1481002", tweet="test3", userid=test_user)
+        Posts.objects.create(postid="1999999", tweet="test4", userid=test_user)
+
+        Posts.objects.all().delete()
+        num_posts = Posts.objects.count()
+
+        self.assertEqual(num_posts, 0)
+
+    def test_update_tweet(self):
+        """
+        Updates one tweet from the database
+        """
+        test_user = User.objects.create(userid="337282", twitterhandle="test1", email="test1@test.com", password="PASSWORD")
+        Posts.objects.create(postid="1485402", tweet="test1", userid=test_user)
+        Posts.objects.create(postid="1487602", tweet="test2", userid=test_user)
+        Posts.objects.create(postid="1481002", tweet="test3", userid=test_user)
+        Posts.objects.create(postid="1999999", tweet="test4", userid=test_user)
+
+        new_post = Posts.objects.get(postid="1481002")
+        new_post.tweet = "welcome to the jungle"
+        new_post.save()
+
+        response = Posts.objects.get(postid="1481002")
+
+        self.assertEqual(response.tweet, "welcome to the jungle")
+
+    def test_update_tweets(self):
+        """
+        Updates two tweets from the database
+        """
+        test_user = User.objects.create(userid="337282", twitterhandle="test1", email="test1@test.com", password="PASSWORD")
+        Posts.objects.create(postid="1485402", tweet="test1", userid=test_user)
+        Posts.objects.create(postid="1487602", tweet="test2", userid=test_user)
+        Posts.objects.create(postid="1481002", tweet="test3", userid=test_user)
+        Posts.objects.create(postid="1999999", tweet="test4", userid=test_user)
+
+        new_post = Posts.objects.get(postid="1481002")
+        new_post.tweet = "welcome to the jungle"
+        new_post.save()
+
+        new_post = Posts.objects.get(postid="1485402")
+        new_post.tweet = "welcome to the crew"
+        new_post.save()
+
+        response1 = Posts.objects.get(postid="1481002")
+        response2 = Posts.objects.get(postid="1485402")
+
+        self.assertEqual(response1.tweet, "welcome to the jungle")
+        self.assertEqual(response2.tweet, "welcome to the crew")
+
+    """
     def test_update_all(self):
-        """
-        Creates and updates the password of every user from the database
-        """
+        test_user = User.objects.create(userid="337282", twitterhandle="test1", email="test1@test.com", password="PASSWORD")
+        Posts.objects.create(postid="1485402", tweet="test1", userid=test_user)
+        Posts.objects.create(postid="1487602", tweet="test2", userid=test_user)
+        Posts.objects.create(postid="1481002", tweet="test3", userid=test_user)
+        Posts.objects.create(postid="1999999", tweet="test4", userid=test_user)
+
+        for obj in Posts:
+            obj.tweet = "this is my tweet"
+            obj.save()
+
+        response1 = User.objects.get(postid="1485402")
+        response2 = User.objects.get(postid="1487602")
+        response3 = User.objects.get(postid="1481002")
+        response4 = User.objects.get(postid="1999999")
+
+        self.assertEqual(response1.status_code, 302)
+        self.assertEqual(response1.tweet, "this is my tweet")
+        self.assertEqual(response2.status_code, 302)
+        self.assertEqual(response2.tweet, "this is my tweet")
+        self.assertEqual(response3.status_code, 302)
+        self.assertEqual(response3.tweet, "this is my tweet")
+        self.assertEqual(response4.status_code, 302)
+        self.assertEqual(response4.tweet, "this is my tweet")
+    
+    def test_update_all(self):
         User.objects.create(
             userid="337282",
             twitterhandle="test1",
@@ -334,7 +505,7 @@ class UserTableTests(TestCase):
             password="PASSWORD",
         )
 
-        for obj in User.objects:
+        for obj in User:
             obj.password = "PA$$WORD"
             obj.save()
 
@@ -347,193 +518,4 @@ class UserTableTests(TestCase):
         self.assertEqual(response2.password, "PA$$WORD")
         self.assertEqual(response3.password, "PA$$WORD")
         self.assertEqual(response4.password, "PA$$WORD")
-        self.assertEqual(response1.status_code, 302)
-        self.assertEqual(response2.status_code, 302)
-        self.assertEqual(response3.status_code, 302)
-        self.assertEqual(response4.status_code, 302)
-
-
-class TweetTableTests(TestCase):
     """
-    These set of tests checks various areas related to the Posts's table.
-    """
-
-    def test_add_tweet(self):
-        """
-        Adds a new tweet to the database
-        """
-        Posts.objects.create(postid="1425002", tweet="test1")
-
-        num_tweets = Posts.objects.count()
-
-        self.assertEqual(num_tweets, 1)
-
-    def test_add_tweets(self):
-        """
-        Adds three new tweets to the database
-        """
-        Posts.objects.create(postid="1425002", tweet="test1", userid_id="337282")
-        Posts.objects.create(postid="1492332", tweet="test2", userid_id="1374553")
-        Posts.objects.create(postid="1482002", tweet="test3", userid_id="337282")
-
-        num_tweets = Posts.objects.count()
-
-        self.assertEqual(num_tweets, 3)
-
-    def test_get_alltweets(self):
-        """
-        Gets all tweets from the database
-        """
-        Posts.objects.create(postid="1482002", tweet="test1", userid_id="337282")
-        Posts.objects.create(postid="1482007", tweet="test2", userid_id="9888853")
-        Posts.objects.create(postid="1425666", tweet="test3", userid_id="9123543")
-
-        response = Posts.objects.get()
-
-        self.assertEqual(response.status_code, 302)
-
-    def test_get_tweet(self):
-        """
-        Gets one tweet from the database
-        """
-        Posts.objects.create(postid="1485402", tweet="test1", userid_id="337282")
-        Posts.objects.create(postid="1487602", tweet="test2", userid_id="337282")
-        Posts.objects.create(postid="1481002", tweet="test3", userid_id="9000253")
-
-        response = Posts.objects.get(tweet="test2")
-
-        self.assertEqual(response.status_code, 302)
-
-        return str(response)
-
-    def test_get_tweets(self):
-        """
-        Gets two tweets from the database
-        """
-        Posts.objects.create(postid="1485402", tweet="test1", userid_id="337282")
-        Posts.objects.create(postid="1487602", tweet="test2", userid_id="337282")
-        Posts.objects.create(postid="1481002", tweet="test3", userid_id="9000253")
-
-        response1 = Posts.objects.get(tweet="test1")
-        response2 = Posts.objects.get(tweet="test3")
-
-        self.assertEqual(response1.status_code, 302)
-        self.assertEqual(response2.status_code, 302)
-
-        return str(response1 + " \n" + response2)
-
-    def test_delete_tweet(self):
-        """
-        Deletes one tweet from the database
-        """
-        Posts.objects.create(postid="1485402", tweet="test1", userid_id="337282")
-        Posts.objects.create(postid="1487602", tweet="test2", userid_id="337282")
-        Posts.objects.create(postid="1481002", tweet="test3", userid_id="9000253")
-        Posts.objects.create(postid="1999999", tweet="test4", userid_id="9111111")
-
-        response = Posts.objects.get(tweet="test1").delete()
-
-        num_posts = Posts.objects.count()
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(num_posts, 3)
-
-    def test_delete_tweets(self):
-        Posts.objects.create(postid="1485402", tweet="test1", userid_id="337282")
-        Posts.objects.create(postid="1487602", tweet="test2", userid_id="337282")
-        Posts.objects.create(postid="1481002", tweet="test3", userid_id="9000253")
-        Posts.objects.create(postid="1999999", tweet="test4", userid_id="9111111")
-
-        response = Posts.objects.get(tweet="test1").delete()
-        response = Posts.objects.get(tweet="test4").delete()
-
-        num_posts = Posts.objects.count()
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(num_posts, 2)
-
-    def test_delete_all(self):
-        """
-        Deletes all tweets from the database
-        """
-        Posts.objects.create(postid="1485402", tweet="test1", userid_id="337282")
-        Posts.objects.create(postid="1487602", tweet="test2", userid_id="337282")
-        Posts.objects.create(postid="1481002", tweet="test3", userid_id="9000253")
-        Posts.objects.create(postid="1999999", tweet="test4", userid_id="9111111")
-
-        response = Posts.objects.get().delete()
-
-        num_posts = Posts.objects.count()
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(num_posts, 0)
-
-    def test_update_tweet(self):
-        """
-        Updates one tweet from the database
-        """
-        Posts.objects.create(postid="1485402", tweet="test1", userid_id="337282")
-        Posts.objects.create(postid="1487602", tweet="test2", userid_id="337282")
-        Posts.objects.create(postid="1481002", tweet="test3", userid_id="9000253")
-        Posts.objects.create(postid="1999999", tweet="test4", userid_id="9111111")
-
-        new_post = Posts.objects.get(postid="1481002")
-        new_post.tweet = "welcome to the jungle"
-        new_post.save()
-
-        response = Posts.objects.get(postid="1481002")
-
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.tweet, "welcome to the jungle")
-
-    def test_update_tweets(self):
-        """
-        Updates two tweets from the database
-        """
-        Posts.objects.create(postid="1485402", tweet="test1", userid_id="337282")
-        Posts.objects.create(postid="1487602", tweet="test2", userid_id="337282")
-        Posts.objects.create(postid="1481002", tweet="test3", userid_id="9000253")
-        Posts.objects.create(postid="1999999", tweet="test4", userid_id="9111111")
-
-        new_post = Posts.objects.get(postid="1481002")
-        new_post.tweet = "welcome to the jungle"
-        new_post.save()
-
-        new_post = Posts.objects.get(postid="1485402")
-        new_post.tweet = "welcome to the crew"
-        new_post.save()
-
-        response1 = Posts.objects.get(postid="1481002")
-        response2 = Posts.objects.get(postid="1485402")
-
-        self.assertEqual(response1.status_code, 302)
-        self.assertEqual(response1.tweet, "welcome to the jungle")
-        self.assertEqual(response2.status_code, 302)
-        self.assertEqual(response2.tweet, "welcome to the crew")
-
-    def test_update_all(self):
-        """
-        Updates all tweets from the database
-        """
-        Posts.objects.create(postid="1485402", tweet="test1", userid_id="337282")
-        Posts.objects.create(postid="1487602", tweet="test2", userid_id="337282")
-        Posts.objects.create(postid="1481002", tweet="test3", userid_id="9000253")
-        Posts.objects.create(postid="1999999", tweet="test4", userid_id="9111111")
-
-        for obj in Posts.objects:
-            obj.tweet = "this is my tweet"
-            obj.save()
-
-        response1 = User.objects.get(postid="1485402")
-        response2 = User.objects.get(postid="1487602")
-        response3 = User.objects.get(postid="1481002")
-        response4 = User.objects.get(postid="1999999")
-
-        self.assertEqual(response1.status_code, 302)
-        self.assertEqual(response1.tweet, "this is my tweet")
-        self.assertEqual(response2.status_code, 302)
-        self.assertEqual(response2.tweet, "this is my tweet")
-        self.assertEqual(response3.status_code, 302)
-        self.assertEqual(response3.tweet, "this is my tweet")
-        self.assertEqual(response4.status_code, 302)
-        self.assertEqual(response4.tweet, "this is my tweet")

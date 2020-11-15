b4b7ca2cb467fc04fffa6d3c9ef8f97f9522004e
Matthew Knop <knopmatthew93@gmail.com>
2020-11-14 21:36:58 -0700

    Fixed urls.

--- tcapi/urls.py
+++ tcapi/urls.py
@@ -7,13 +7,10 @@ from .views import current_user, UserList
 urlpatterns = [
     path('current_user/', current_user),
     path('users/', UserList.as_view()),
-    #url(r"^api/users", views.user_list),
-    #url(r"^api/users/(?P<pk>[0-9]+)$", views.user_detail),
     url(r"^api/posts", views.tweet_list),
     url(r"^api/individualposts/(?P<pk>[0-9]+)$", views.tweet_detail),
     url(r"^api/postreactions", views.postreactions_list),
     url(r"^api/postreactions/(?P<pk>[0-9]+)$", views.postreactions_detail),
     url(r"^api/commentreplies", views.commentreplies_list),
     url(r"^api/commentreplies/(?P<pk>[0-9]+)$", views.commentreplies_detail),
-
 ]

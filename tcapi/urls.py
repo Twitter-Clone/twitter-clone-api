4d4f5795cb97fa400ddf26883f3c8f0192f37aa1
Matthew Knop <knopmatthew93@gmail.com>
2020-11-14 20:33:22 -0700

    Testing.

--- tcapi/urls.py
+++ tcapi/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     #url(r"^api/users", views.user_list),
     #url(r"^api/users/(?P<pk>[0-9]+)$", views.user_detail),
     url(r"^api/posts", views.tweet_list),
-    url(r"^api/posts/(?P<pk>[0-9]+)$", views.tweet_detail),
+    url(r"^api/individualposts/(?P<pk>[0-9]+)$", views.tweet_detail),
     url(r"^api/postreactions", views.postreactions_list),
     url(r"^api/postreactions/(?P<pk>[0-9]+)$", views.postreactions_detail),
     url(r"^api/commentreplies", views.commentreplies_list),

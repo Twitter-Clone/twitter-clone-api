b809305c410931462f308c4922a17b5efa5382ea
Matthew Knop <knopmatthew93@gmail.com>
2020-11-15 20:26:19 -0700

    Fixed endpoint name.

--- tcapi/urls.py
+++ tcapi/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('users/', UserList.as_view()), # Registration endpoint
     url(r'^api/users/', views.user_list), # Get all user(s)
     url(r"^api/posts", views.tweet_list), # Get all posts
-    url(r"^api/posts", views.post_tweet_auth_user), # Post new tweet
+    url(r"^api/postnew", views.post_tweet_auth_user), # Post new tweet
     url(r"^api/individualposts/(?P<pk>[0-9]+)$", views.tweet_detail),
     url(r"^api/postreactions", views.postreactions_list),
     url(r"^api/postreactions/(?P<pk>[0-9]+)$", views.postreactions_detail),
